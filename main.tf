2d040b9ff62fb565657e95e9891bc9201cce8e19
Abhishek Gupta <91731524+AbhiGupta8295@users.noreply.github.com>
2023-06-16 21:04:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,13 @@ resource "aws_instance" "example" {
   ami           = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"
   count         = 2
+  key_name = "tfServer"
+  vpc_security_group_ids = [ "sg-0454b7e51120799e6" ]
 
   tags = {
     Name = "nginx-server-${count.index}"
   }
+}
+output "instance_ips" {
+  value = aws_instance.example[*].public_ip
 }
